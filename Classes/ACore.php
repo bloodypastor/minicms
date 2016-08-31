2b18f1b2454cd9c825a884def1cfd1c6ea1cb515
bloodypastor <bloodypastor7@gmail.com>
2016-09-01 00:50:55 +0300

    add category

--- Classes/ACore.php
+++ Classes/ACore.php
@@ -13,7 +13,7 @@ abstract class ACore {
         if (!mysqli_select_db($this->db, DB)){
             exit("Немає такої бази даних" . mysqli_error());
         }
-        mysqli_query("SET NAMES 'UTF8'");
+
     }
 
     protected function get_header(){
@@ -21,13 +21,29 @@ abstract class ACore {
     }
 
     protected function get_left_bar(){
+        $query = "SELECT id_category,name_category FROM category";
 
+        $result = mysqli_query($query);
+        if (!$result){
+            exit(mysqli_error());
+        }
+
+        $row = array();
+        echo '<div class="quick-bg">
+			<div id="spacer" style="margin-top:15px;">
+				<div id="rc-bg">Menu</div>
+			</div>';
+        for ($i = 0; $i < mysqli_num_rows($result); $i++){
+            $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            printf("<div class='quick-links'> <a href='?option=category&id_cat=%s'>%s</a></div>", $row['id_category'], $row['name_category']);
+        }
     }
 
 
 
     public function get_body(){
         $this->get_header();
+        $this->get_left_bar();
     }
 }
 
